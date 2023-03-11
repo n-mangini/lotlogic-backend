c37fdc362a21f396a9d87c690ffb3c0281328f2d
nicom <nicolas.mangini@ing.austral.edu.ar>
2023-03-10 23:33:19 -0300

    add: user POST api, isOwner() & findUserByDni() method

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -13,8 +13,19 @@ public class User {
     private String name;
     private String surname;
     private String password;
+    @Transient
+    private boolean isOwner;
 
     public User() {
+    }
+
+    public User(int id, String dni, String name, String surname, String password, boolean isOwner) {
+        this.id = id;
+        this.dni = dni;
+        this.name = name;
+        this.surname = surname;
+        this.password = password;
+        this.isOwner = isOwner;
     }
 
     public User(String dni, String name, String surname, String password) {
@@ -22,5 +33,30 @@ public class User {
         this.name = name;
         this.surname = surname;
         this.password = password;
+        this.isOwner = isOwner();
+    }
+
+    public String getDni() {
+        return this.dni;
+    }
+
+    public boolean isOwner() {
+        return getPassword().length() > 4;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", dni='" + dni + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", password='" + password + '\'' +
+                ", isOwner=" + isOwner +
+                '}';
     }
 }
--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,8 +1,10 @@
 package com.example.demo.user;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,11 @@ public class UserService {
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    public void addNewUser(@RequestBody User user) {
+        Optional<User> userOptional = userRepository.findUserByDni(user.getDni());
+        if (userOptional.isPresent()) throw new IllegalStateException("El usuario ya existe");
+        userRepository.save(user);
     }
 }
